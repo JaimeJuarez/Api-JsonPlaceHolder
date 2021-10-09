055d9f4a4759090cfb40c2b6e727f2ecf0e280a7
JaimeJuarez <jjuarez17@ucol.mx>
2021-10-08 23:57:28 -0500

    Api-JsonPlaceHolder V2

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var jsonbody = "";
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(jsondata => {
+        console.log(jsondata);
         for (let i = 0; i < jsondata.length; i++) {
             jsonbody += `<option value = "${jsondata[i].id}"> ${jsondata[i].name} </option>`
         }
@@ -14,12 +15,12 @@ fetch('https://jsonplaceholder.typicode.com/users')
 users.addEventListener("change", () => {
     var id = document.getElementById("user-select").value
     fetch("https://jsonplaceholder.typicode.com/posts?userId=" + id)
-        .then((response) => response.json())
+        .then((res) => res.json())
         .then((json) => {
             var data = "";
             for (let i = 0; i < json.length; i++) {
                 console.log(json);
-                data += `<div> <h3>${json[i].id}.- ${json[i].title}</h3> <p>${json[i].body}</p> </div>`
+                data += `<div> <h3>${json[i].id}.- ${json[i].title}</h3> <p>${json[i].body}</p>  <button class="btn btn-primary">Ver Comentarios</button> <div id="coments${json[i].id}"></div> </div>`
             }
             demo.innerHTML = data;
         })
