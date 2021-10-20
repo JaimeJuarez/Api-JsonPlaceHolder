bd79655443738ad7e056703135ea020a1fbe9337
Jaime <jjuarez17@ucol.mx>
2021-10-20 18:49:46 -0500

    Programa finalizado

--- js/app.js
+++ js/app.js
@@ -1,26 +1,51 @@
-var demo = document.getElementById("demo");
-var users = document.getElementById("user-select")
-var jsonbody = "";
-fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(jsondata => {
-        for (let i = 0; i < jsondata.length; i++) {
-            jsonbody += `<option value = "${jsondata[i].id}"> ${jsondata[i].name} </option>`
-        }
-        users.innerHTML = jsonbody;
+var btndata = document.getElementById("btn-showdata").onclick = showData;
+
+function showData() {
+    // btnuser.removeAttribute("hidden");
+    var demo = document.getElementById("demo");
+    var userdata = document.getElementById("user-data");
+    var users = document.getElementById("user-select");
+    var jsonbody = "";
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(jsondata => {
+            for (let i = 0; i < jsondata.length; i++) {
+                jsonbody += `<option value = "${jsondata[i].id}"> ${jsondata[i].name} </option>`
+            }
+            users.innerHTML = jsonbody;
+        })
+
+
+    users.addEventListener("change", () => {
+        var id = document.getElementById("user-select").value
+        fetch("https://jsonplaceholder.typicode.com/posts?userId=" + id)
+            .then((response) => response.json())
+            .then((json) => {
+                var data = "";
+                for (let i = 0; i < json.length; i++) {
+                    data += `<div> <h3>${json[i].id}.- ${json[i].title}</h3> <p>${json[i].body}</p> </div>`
+                }
+                demo.innerHTML = data;
+            })
     })
 
 
-users.addEventListener("change", () => {
-    var id = document.getElementById("user-select").value
-    fetch("https://jsonplaceholder.typicode.com/posts?userId=" + id)
-        .then((response) => response.json())
-        .then((json) => {
-            var data = "";
-            for (let i = 0; i < json.length; i++) {
+
+    // function mostrarUserData() {
+
+    users.addEventListener("change", () => {
+        var id2 = document.getElementById("user-select").value
+        fetch("https://jsonplaceholder.typicode.com/users?id=" + id2)
+            .then((response) => response.json())
+            .then((json) => {
+                var usersdata = "";
+                for (let i = 0; i < json.length; i++) {
+                    usersdata = "Nombre: " + json[i].name + "<br>" + "Username: " + json[i].username + "<br>" + "Email: " + json[i].email + "<br>" + "Phone: " + json[i].phone;
+                }
+
+                userdata.innerHTML = usersdata;
                 console.log(json);
-                data += `<div> <h3>${json[i].id}.- ${json[i].title}</h3> <p>${json[i].body}</p> </div>`
-            }
-            demo.innerHTML = data;
-        })
-})
+            })
+    })
+}
+// }
